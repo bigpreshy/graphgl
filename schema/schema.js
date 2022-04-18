c0d9e5539e17dd21fd53e09002590d33896dade7
Presh <lukeprecious@gmail.com>
2022-04-18 21:03:24 +0100

    Destructured graphqlHTTP

--- schema/schema.js
+++ schema/schema.js
@@ -1,39 +1,31 @@
-const graphql = require('graphql');
-const _ = require('lodash');
-const {countries} = require('./country');
+const graphql = require("graphql");
+const _ = require("lodash");
+const { countries } = require("./country");
 
-
-const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLID
-} = graphql;
-
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 const CountryType = new GraphQLObjectType({
-    name: 'Country',
-    fields: ( ) => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        capital: { type: GraphQLString }
-    })
+  name: "Country",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    capital: { type: GraphQLString },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        country: {
-            type: CountryType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-              
-                return _.find(countries, { id: args.id });
-            }
-        }
-    }
+  name: "RootQueryType",
+  fields: {
+    country: {
+      type: CountryType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(countries, { id: args.id });
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
